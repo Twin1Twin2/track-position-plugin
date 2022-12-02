1e17e30a4665709f3e0b03352204fd99dd5eae3f
Twin1Twin2 <theawesometwin827@gmail.com>
2022-12-02 05:00:48 -1000

    Better fit in frame

--- src/app/app.story.lua
+++ src/app/app.story.lua
@@ -12,7 +12,56 @@ local app = require(script.Parent.app)
 local stateModule = require(script.Parent.state)
 
 local widgets = script.Parent.widgets
-local container = require(widgets.container)
+
+local container = plasma.widget(function(fn)
+	local refs = plasma.useInstance(function(ref)
+		local frame = plasma.create("Frame", {
+			[ref] = "containerFrame",
+			BackgroundTransparency = 1,
+			Position = UDim2.new(0, 0, 0, 0),
+			Size = UDim2.new(1, 0, 1, 0),
+
+			plasma.create("UIPadding", {
+				PaddingTop = UDim.new(0, 10),
+				PaddingBottom = UDim.new(0, 10),
+				PaddingLeft = UDim.new(0, 10),
+				PaddingRight = UDim.new(0, 10),
+			}),
+
+			plasma.create("UIListLayout", {
+				[ref] = "listLayout",
+
+				SortOrder = Enum.SortOrder.LayoutOrder,
+				FillDirection = Enum.FillDirection.Vertical,
+				Padding = UDim.new(0, 10),
+			}),
+		})
+
+		return frame
+	end)
+
+	plasma.scope(fn)
+
+	return refs.frame
+end)
+
+local appFrame = plasma.widget(function(fn)
+	local refs = plasma.useInstance(function(ref)
+		local frame = plasma.create("Frame", {
+			[ref] = "containerFrame",
+			BackgroundTransparency = 1,
+			Position = UDim2.new(0, 0, 0, 0),
+			Size = UDim2.new(1, 0, 1, -60),
+		})
+
+		return frame
+	end)
+
+	plasma.scope(fn)
+
+	return refs.frame
+end)
+
 
 return function(frame: Frame): () -> ()
 	local store = rodux.Store.new(stateModule.reducer, nil, {
@@ -34,7 +83,9 @@ return function(frame: Frame): () -> ()
 				plasma.space()
 
 				if pluginEnabled then
-					app(store)
+					appFrame(function()
+						app(store)
+					end)
 				end
 			end)
 		end)
